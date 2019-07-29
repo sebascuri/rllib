69c5337aae2f95ea27afc89cbcd66d7fcfa4fe32
scuri <sebastian.curi@inf.ethz.ch>
2019-07-29 15:25:01 +0200

    Fix layers in NN parametrization

--- rllib/util/neural_networks/deterministic.py
+++ rllib/util/neural_networks/deterministic.py
@@ -3,9 +3,17 @@ import torch.nn as nn
 
 class DeterministicNN(nn.Module):
     def __init__(self, in_dim, out_dim, layers=None):
-        super(DeterministicNN, self).__init__()
-        self._layer = nn.Linear(in_dim, out_dim)
+        super().__init__()
+        layers_ = []
+        if layers is None:
+            layers = []
+        for layer in layers:
+            layers_.append(nn.Linear(in_dim, layer))
+            layers_.append(nn.ReLU())
+            in_dim = layer
 
-    def forward(self, input_):
-        output = self._layer(input_)
-        return output
+        self._layers = nn.Sequential(*layers_)
+        self._head = nn.Linear(in_dim, out_dim)
+
+    def forward(self, x):
+        return self._head(self._layers(x))
