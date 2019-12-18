f5526878eb92f9bcabef45a4d655bdaef404cffc
scuri <sebastian.curi@inf.ethz.ch>
2019-12-18 15:00:37 +0100

    Avoid repetions in rollout

--- rllib/util/rollout.py
+++ rllib/util/rollout.py
@@ -2,18 +2,25 @@
 
 import torch
 from rllib.dataset import Observation
-from rllib.agent import AbstractAgent
-from rllib.environment import AbstractEnvironment
-from rllib.policy import AbstractPolicy
-from typing import List
 
 
 __all__ = ['rollout_agent', 'rollout_policy']
 
 
-def rollout_agent(environment: AbstractEnvironment, agent: AbstractAgent,
-                  num_episodes: int = 1, max_steps: int = 1000, render: bool = False
-                  ) -> None:
+def _step(environment, state, action, render):
+    next_state, reward, done, _ = environment.step(action)
+    observation = Observation(state=state,
+                              action=action,
+                              reward=reward,
+                              next_state=next_state,
+                              done=done)
+    state = next_state
+    if render:
+        environment.render()
+    return observation, state, done
+
+
+def rollout_agent(environment, agent, num_episodes=1, max_steps=1000, render=False):
     """Conduct a single rollout of an agent in an environment.
 
     Parameters
@@ -22,7 +29,7 @@ def rollout_agent(environment: AbstractEnvironment, agent: AbstractAgent,
     agent: AbstractAgent
     num_episodes: int, optional (default=1)
     max_steps: int.
-    render:bool.
+    render: bool.
 
     """
     for _ in range(num_episodes):
@@ -31,29 +38,15 @@ def rollout_agent(environment: AbstractEnvironment, agent: AbstractAgent,
         done = False
         while not done:
             action = agent.act(state)
-            next_state, reward, done, _ = environment.step(action)
-            max_time = max_steps <= environment.time
-            if done:
-                print(environment.time)
-            observation = Observation(state=state,
-                                      action=action,
-                                      reward=reward,
-                                      next_state=next_state,
-                                      done=done)
+            observation, state, done = _step(environment, state, action, render)
             agent.observe(observation)
-            state = next_state
-            if render:
-                environment.render()
-
-            if max_time:
+            if max_steps <= environment.time:
                 break
         agent.end_episode()
     agent.end_interaction()
 
 
-def rollout_policy(environment: AbstractEnvironment, policy: AbstractPolicy,
-                   num_episodes: int = 1, max_steps: int = 1000, render: bool = False
-                   ) -> List[List[Observation]]:
+def rollout_policy(environment, policy, num_episodes=1, max_steps=1000, render=False):
     """Conduct a single rollout of a policy in an environment.
 
     Parameters
@@ -66,7 +59,7 @@ def rollout_policy(environment: AbstractEnvironment, policy: AbstractPolicy,
 
     Returns
     -------
-    trajectory: list of Observation
+    trajectories: list of list of Observation.
 
     """
     trajectories = []
@@ -76,21 +69,10 @@ def rollout_policy(environment: AbstractEnvironment, policy: AbstractPolicy,
         trajectory = []
         with torch.no_grad():
             while not done:
-                state_torch = torch.tensor(state).float()
-                action = policy(state_torch).sample().numpy()
-                next_state, reward, done, _ = environment.step(action)
-                max_time = max_steps <= environment.time
-                observation = Observation(state=state,
-                                          action=action,
-                                          reward=reward,
-                                          next_state=next_state,
-                                          done=done)
+                action = policy(torch.tensor(state).float()).sample().numpy()
+                observation, state, done = _step(environment, state, action, render)
                 trajectory.append(observation)
-                state = next_state
-
-                if render:
-                    environment.render()
-                if max_time:
+                if max_steps <= environment.time:
                     break
         trajectories.append(trajectory)
     return trajectories
---
+++ rllib/util/rollout.pyi
@@ -0,0 +1,13 @@
+from rllib.agent.abstract_agent import AbstractAgent
+from rllib.policy.abstract_policy import AbstractPolicy
+from rllib.environment.abstract_environment import AbstractEnvironment
+from typing import List
+from rllib.dataset import Observation
+
+def rollout_agent(environment: AbstractEnvironment, agent: AbstractAgent,
+                  num_episodes: int = 1, max_steps: int = 1000, render: bool = False
+                  ) -> None: ...
+
+def rollout_policy(environment: AbstractEnvironment, policy: AbstractPolicy,
+                  num_episodes: int = 1, max_steps: int = 1000, render: bool = False
+                  ) -> List[List[Observation]]: ...
