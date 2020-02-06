b3f4ec02f9a69eea7ff448b9eda4c0c9e304b0b5
Sebastian Curi <sebascuri@gmail.com>
2020-02-06 11:49:35 +0100

    Add stub definition of step

--- rllib/util/rollout.pyi
+++ rllib/util/rollout.pyi
@@ -1,8 +1,15 @@
 from rllib.agent.abstract_agent import AbstractAgent
 from rllib.policy.abstract_policy import AbstractPolicy
 from rllib.environment.abstract_environment import AbstractEnvironment
-from typing import List
+from typing import List, Union, Tuple
 from rllib.dataset import Observation
+from numpy import ndarray
+
+
+def _step(environment: AbstractEnvironment, state: Union[int, ndarray],
+          action: Union[int, ndarray], render: bool
+          ) -> Tuple[Observation, Union[int, ndarray], bool]:...
+
 
 def rollout_agent(environment: AbstractEnvironment, agent: AbstractAgent,
                   num_episodes: int = 1, max_steps: int = 1000, render: bool = False,
