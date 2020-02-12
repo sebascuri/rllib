Add Mellowmax policy & Fix bug in softmax

rllib/policy/q_function_policy/softmax.py
@@ -8,12 +8,8 @@ import torch
 class SoftMax(AbstractQFunctionPolicy):
     """Implementation of Softmax Policy.
 
-    An epsilon greedy exploration strategy chooses the greedy strategy with probability
-    1-epsilon, and a random action with probability epsilon.
-
-    If eps_end and eps_decay are not set, then epsilon will be always eps_start.
-    If not, epsilon will decay exponentially at rate eps_decay from eps_start to
-    eps_end.
+    A soft-max policy is one that has a policy given by:
+        pi(a|s) propto exp[q(s, a)]
 
     """
 
@@ -21,4 +17,4 @@ class SoftMax(AbstractQFunctionPolicy):
         """See `AbstractQFunctionPolicy.__call__'."""
         q_val = self.q_function(state)
         temperature = self.param()
-        return Categorical(torch.softmax(q_val / temperature, dim=0))
+        return Categorical(torch.softmax(q_val / temperature, dim=-1))

rllib/util/utilities.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 from torch.distributions import constraints
 from torch.distributions import Distribution
-__all__ = ['mc_value', 'sum_discounted_rewards', 'Delta']
+__all__ = ['mc_value', 'sum_discounted_rewards', 'Delta', 'mellow_max']
 
 
 def _mc_value_slow(trajectory, gamma=1.0):
@@ -76,6 +76,28 @@ def sum_discounted_rewards(trajectory, gamma):
     rewards = np.array(rewards)
     i = np.arange(len(rewards))
     return np.sum(rewards * np.power(gamma, i))
+
+
+def mellow_max(values, omega=1.):
+    """Find mellow-max of an array of values.
+
+    The mellow max is log(1/n sum(e^x)).
+
+    Parameters
+    ----------
+    values: Array.
+        array of values to find mellow max.
+    omega: float, optional (default=1.).
+        parameter of mellow-max.
+
+    References
+    ----------
+    Asadi, Kavosh, and Michael L. Littman.
+    "An alternative softmax operator for reinforcement learning."
+    Proceedings of the 34th International Conference on Machine Learning. 2017.
+    """
+    n = torch.tensor(values.shape[-1]).float()
+    return (torch.logsumexp(omega * values, dim=-1) - torch.log(n)) / omega
 
 
 class Delta(Distribution):