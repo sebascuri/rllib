5e794b6bcccfbbc2c5a78ce9e7347344b88f6934
Sebastian Curi <sebascuri@gmail.com>
2020-02-14 15:07:52 +0100

    Add noise smoothing to DDPG agent too

--- rllib/agent/ddpg_agent/ddpg_agent.py
+++ rllib/agent/ddpg_agent/ddpg_agent.py
@@ -1,5 +1,6 @@
 """Implementation of DDPG Algorithm."""
 from .abstract_dpg_agent import AbstractDPGAgent
+import torch
 
 
 class DDPGAgent(AbstractDPGAgent):
@@ -16,8 +17,12 @@ class DDPGAgent(AbstractDPGAgent):
     def _td(self, state, action, reward, next_state, done):
         pred_q = self.q_function(state, action)
 
-        # target = r + gamma * Q_target(x', \pi_target(x'))
+        # target = r + gamma * Q_target(x', \pi_target(x') + noise)
         next_policy_action = self.policy_target(next_state).mean
+        next_action_noise = (torch.randn_like(next_policy_action) * self.policy_noise
+                             ).clamp(-self.noise_clip, self.noise_clip)
+        next_policy_action = (next_policy_action + next_action_noise).clamp(-1, 1)
+
         next_q = self.q_target(next_state, next_policy_action)
         target_q = reward + self.gamma * next_q * (1 - done)
 
