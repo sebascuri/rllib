6c8c6ce96ce980a24a0187848cf81f1e9b90da4b
scuri <sebastian.curi@inf.ethz.ch>
2020-03-25 16:44:51 +0100

    Add an action cost to Pendulum Reward model.

--- rllib/reward/pendulum_reward.py
+++ rllib/reward/pendulum_reward.py
@@ -9,13 +9,27 @@ from .utilities import tolerance
 class PendulumReward(AbstractReward):
     """Reward for Inverted Pendulum."""
 
+    def __init__(self, action_cost_ratio=0):
+        super().__init__()
+        self.action_cost_ratio = action_cost_ratio
+
     def forward(self, state, action):
         """See `abstract_reward.forward'."""
         if not isinstance(state, torch.Tensor):
             state = torch.tensor(state, dtype=torch.get_default_dtype())
+        if not isinstance(action, torch.Tensor):
+            action = torch.tensor(action, dtype=torch.get_default_dtype())
+
         cos_angle = torch.cos(state[..., 0])
         velocity = state[..., 1]
 
         angle_tolerance = tolerance(cos_angle, lower=0.95, upper=1., margin=0.1)
         velocity_tolerance = tolerance(velocity, lower=-.5, upper=0.5, margin=0.5)
-        return angle_tolerance * velocity_tolerance, torch.zeros(1)
+        state_cost = angle_tolerance * velocity_tolerance
+
+        action_tolerance = tolerance(action[..., 0], lower=-0.1, upper=0.1, margin=0.1)
+        action_cost = self.action_cost_ratio * action_tolerance
+
+        cost = state_cost + action_cost
+
+        return cost, torch.zeros(1)
