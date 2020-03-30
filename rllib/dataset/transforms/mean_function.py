06aea701db3e5decef2c1043b908e6cb29102021
scuri <sebastian.curi@inf.ethz.ch>
2020-03-30 21:05:02 +0200

    Fix bug in mean_function inverse calculation

--- rllib/dataset/transforms/mean_function.py
+++ rllib/dataset/transforms/mean_function.py
@@ -51,17 +51,11 @@ class MeanFunction(AbstractTransform):
     def inverse(self, observation: Observation):
         """See `AbstractTransform.inverse'."""
         mean_next_state = self.mean_function(observation.state, observation.action)
-        if isinstance(observation.next_state, tuple):
-            next_state = (observation.next_state[0] + mean_next_state,
-                          observation.next_state[1])
-
-        else:
-            next_state = observation.next_state + mean_next_state
         return Observation(
             state=observation.state,
             action=observation.action,
             reward=observation.reward,
-            next_state=next_state,
+            next_state=observation.next_state + mean_next_state,
             done=observation.done,
             next_action=observation.next_action,
             log_prob_action=observation.log_prob_action,
