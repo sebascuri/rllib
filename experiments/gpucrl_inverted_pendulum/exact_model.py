064c2a4c2c5f4748451516781228eb54437ab58c
scuri <sebastian.curi@inf.ethz.ch>
2020-04-27 18:53:31 +0200

    Add boilerplate code for hyper_param search

--- experiments/gpucrl_inverted_pendulum/exact_model.py
+++ experiments/gpucrl_inverted_pendulum/exact_model.py
@@ -27,10 +27,12 @@ num_iter = 100
 num_sim_steps = 400
 num_gradient_steps = 50
 # (1.0, 1.9000000000000004, 0.5000000000000001)
-
-best_returns = -float('Inf')
-best_params = None
-results = {}
+# best_returns = -float('Inf')
+# best_params = None
+# for eta, eta_mean, eta_var in product([1.8, 2.0],
+#                                       [1.1, 1.3, 1.5, 1.7, 1.9],
+#                                       np.arange(0.3, 1.9, 0.2)):
+#     results = {}
 returns = solve_mpc(
     dynamic_model, action_cost_ratio=action_cost_ratio,
     num_iter=num_iter, num_sim_steps=num_sim_steps, batch_size=batch_size,
@@ -39,4 +41,15 @@ returns = solve_mpc(
     epsilon=epsilon, epsilon_mean=epsilon_mean, epsilon_var=epsilon_var,
     eta=eta, eta_mean=eta_mean, eta_var=eta_var,
     lr=lr)
+# params = (eta, eta_mean, eta_var)
+# results[params] = returns
+#
+# if returns > best_returns:
+#     best_returns = returns
+#     best_params = params
 
+# best = {k: v for k, v in filter(lambda item: item[1] > 200,
+#                                 sorted(results.items(), key=lambda item: item[1]))}
+#
+# for i in range(3):
+#     print(np.unique([k[i] for k in best.keys()], return_counts=True))
