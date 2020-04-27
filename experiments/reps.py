63276d37bb6640017df76f93f34c5c134ea169b7
scuri <sebastian.curi@inf.ethz.ch>
2020-04-27 18:51:17 +0200

    Add agent in REPS experiment

--- experiments/reps.py
+++ experiments/reps.py
@@ -1,92 +1,51 @@
 """Python Script Template."""
-from rllib.algorithms.reps import REPS
+from rllib.agent.reps_agent import REPSAgent
+from rllib.algorithms.reps import REPS, QREPS
 from rllib.environment import GymEnvironment
 from rllib.policy import NNPolicy
-from rllib.value_function import NNValueFunction
+from rllib.value_function import NNValueFunction, NNQFunction
 from rllib.dataset.experience_replay import ExperienceReplay
+from rllib.util.training import train_agent, evaluate_agent
 
-from rllib.dataset.utilities import stack_list_of_tuples
-from rllib.util.rollout import step
-from rllib.util.neural_networks.utilities import disable_gradient
-from rllib.util.utilities import tensor_to_distribution
-from rllib.util.parameter_decay import Constant, PolynomialDecay
 import numpy as np
 import torch
 
-import matplotlib.pyplot as plt
-from torch.utils.data import DataLoader
-
-exp_idx = 1
-EPSILON = [0.1, None, None][exp_idx]
-ETA = [None, Constant(0.5), PolynomialDecay(1., 10, 2.)][exp_idx]
+# ETA = 0.5
+ETA = 2.
 NUM_EPISODES = 100
 BATCH_SIZE = 100
-MAX_ITER = 200
 LR = 1e-4
-NUM_ITER = int(200)
+NUM_ITER = 200
+NUM_ACTION_SAMPLES = 15
 NUM_ROLLOUTS = 15
 
 GAMMA = 1
 SEED = 0
+ENVIRONMENT = 'CartPole-v0'
+MAX_STEPS = 200
 
 torch.manual_seed(SEED)
 np.random.seed(SEED)
-env = GymEnvironment('CartPole-v0', SEED)
-policy = NNPolicy(env.dim_state, env.dim_action, env.num_states, env.num_actions,
-                  layers=[64, 64])
-value_function = NNValueFunction(env.dim_state, env.num_states, layers=[64, 64])
 
-reps_loss = REPS(policy, value_function, epsilon=EPSILON, eta=ETA, gamma=GAMMA)
+environment = GymEnvironment(ENVIRONMENT, SEED)
+policy = NNPolicy(environment.dim_state, environment.dim_action,
+                  environment.num_states, environment.num_actions,
+                  layers=[64, 64])
+value_function = NNValueFunction(environment.dim_state, environment.num_states,
+                                 layers=[64, 64])
+q_function = NNQFunction(environment.dim_state, environment.dim_action,
+                         environment.num_states, environment.num_actions,
+                         layers=[64, 64])
+
+reps_loss = REPS(policy, value_function, eta=ETA, gamma=GAMMA)
+# reps_loss = QREPS(policy, value_function, q_function,
+#                   num_action_samples=NUM_ACTION_SAMPLES, eta=ETA, gamma=GAMMA)
 optimizer = torch.optim.Adam(reps_loss.parameters(), lr=LR,  weight_decay=0)
 
-memory = ExperienceReplay(max_len=int(NUM_ROLLOUTS * MAX_ITER))
-total_rewards = []
-losses_ = []
+memory = ExperienceReplay(max_len=int(NUM_ROLLOUTS * MAX_STEPS))
+agent = REPSAgent(ENVIRONMENT, reps_loss, optimizer, memory,
+                  num_iter=NUM_ITER, num_rollouts=NUM_ROLLOUTS, batch_size=BATCH_SIZE,
+                  gamma=GAMMA)
 
-for i_episode in range(NUM_EPISODES):
-    state = env.reset()
-    done = False
-
-    rewards = 0
-    i = 0
-    while not done:
-        i += 1
-        pi = tensor_to_distribution(
-            policy(torch.tensor(state, dtype=torch.get_default_dtype())))
-        action = pi.sample().numpy()
-        observation, state, done = step(env, state, action, pi, render=False)
-        memory.append(observation)
-        rewards += observation.reward.item()
-        if i > MAX_ITER:
-            break
-
-    total_rewards.append(rewards)
-    print(rewards)
-
-    if i_episode % NUM_ROLLOUTS == NUM_ROLLOUTS - 1:
-        loader = DataLoader(memory, batch_size=BATCH_SIZE, shuffle=True)
-        for i in range(NUM_ITER):
-            for obs, idx, weight in loader:
-                loss = reps_loss(obs.state, obs.action, obs.reward, obs.next_state,
-                                 obs.done)
-                optimizer.zero_grad()
-                loss.dual.backward()
-                optimizer.step()
-
-        print(i_episode, loss.eta.item())
-
-        for i in range(NUM_ITER):
-            for obs, idx, weight in loader:
-                loss = reps_loss(obs.state, obs.action, obs.reward, obs.next_state,
-                                 obs.done)
-
-                optimizer.zero_grad()
-                loss.policy_nll.backward()
-                optimizer.step()
-
-        memory.reset()
-
-        reps_loss.update_eta()
-
-plt.plot(total_rewards)
-plt.show()
+train_agent(agent, environment, NUM_EPISODES, MAX_STEPS + 1)
+evaluate_agent(agent, environment, 1, MAX_STEPS + 1)
