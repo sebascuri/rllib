c30cccee06c1d7f2c4bf59c22496fa5490cf19e2
scuri <sebastian.curi@inf.ethz.ch>
2020-05-18 14:21:58 +0200

    Update MB-MPPO with best hparams

--- exps/gpucrl/cart_pole/mbmppo.py
+++ exps/gpucrl/cart_pole/mbmppo.py
@@ -6,16 +6,25 @@ from exps.gpucrl.mb_mppo_arguments import parser
 from exps.gpucrl.plotters import plot_last_sim_and_real_trajectory
 from exps.gpucrl.util import train_and_evaluate
 
-PLAN_HORIZON, SIM_TRAJECTORIES = 4, 8
+PLAN_HORIZON = 4
+MPPO_ETA, MPPO_ETA_MEAN, MPPO_ETA_VAR = 0.5, .7, 5.0
+MPPO_NUM_ITER = 200
+SIM_TRAJECTORIES = 8
+SIM_EXP_TRAJECTORIES = 32
+SIM_NUM_STEPS = ENVIRONMENT_MAX_STEPS
 
 parser.description = 'Run Cart-Pole using Model-Based MPPO.'
 parser.set_defaults(action_cost=ACTION_COST,
                     train_episodes=TRAIN_EPISODES,
                     environment_max_steps=ENVIRONMENT_MAX_STEPS,
                     plan_horizon=PLAN_HORIZON,
-                    sim_num_steps=ENVIRONMENT_MAX_STEPS,
-                    sim_initial_states_num_trajectories=SIM_TRAJECTORIES // 2,
-                    sim_initial_dist_num_trajectories=SIM_TRAJECTORIES // 2,
+                    mppo_num_iter=MPPO_NUM_ITER,
+                    mppo_eta=MPPO_ETA,
+                    mppo_eta_mean=MPPO_ETA_MEAN,
+                    mppo_eta_var=MPPO_ETA_VAR,
+                    sim_num_steps=SIM_NUM_STEPS,
+                    sim_initial_states_num_trajectories=SIM_TRAJECTORIES,
+                    sim_initial_dist_num_trajectories=SIM_EXP_TRAJECTORIES,
                     model_kind='DeterministicEnsemble',
                     model_learn_num_iter=50,
                     model_opt_lr=1e-3)
