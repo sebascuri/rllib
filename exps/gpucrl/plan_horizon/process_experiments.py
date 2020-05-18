dd8b4b77343768357e24bca54f478d2ce8c78308
scuri <sebastian.curi@inf.ethz.ch>
2020-05-18 14:20:31 +0200

    Add plan horizon experiments

--- exps/gpucrl/plan_horizon/process_experiments.py
+++ exps/gpucrl/plan_horizon/process_experiments.py
@@ -1,1 +1,37 @@
 """Python Script Template."""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from exps.gpucrl.util import parse_results
+
+
+base_dir = 'runs/Cartpoleenv'
+df = parse_results(base_dir, 'MBMPPO')
+
+plan_horizon = df.plan_horizon.unique()
+
+fig, axes = plt.subplots(1, 2, sharey='row')
+for h in plan_horizon:
+    mean = df[df.plan_horizon == h].best_return.mean(level=0)
+    std = df[df.plan_horizon == h].best_return.std(level=0).fillna(0)
+    axes[0].plot(mean, label=f"Plan Horizon {h}")
+    axes[0].fill_between(np.arange(len(mean)), mean-std, mean+std, alpha=0.1)
+
+for h in plan_horizon:
+    mean = df[df.plan_horizon == h].model_return.mean(level=0)
+    std = df[df.plan_horizon == h].model_return.std(level=0).fillna(0)
+    axes[1].plot(mean, label=f"Plan Horizon {h}")
+    axes[1].fill_between(np.arange(len(mean)), mean - std, mean + std, alpha=0.1)
+
+
+axes[0].set_ylabel('Returns')
+axes[0].set_xlabel('Num Episode')
+axes[1].set_xlabel('Num Episode')
+
+axes[0].set_title('Environment Return')
+axes[1].set_title('Model Return')
+
+axes[1].legend(loc='best')
+
+fig.tight_layout()
+plt.show()
--- exps/gpucrl/plan_horizon/run_experiments.py
+++ exps/gpucrl/plan_horizon/run_experiments.py
@@ -1,1 +1,17 @@
 """Python Script Template."""
+
+from lsf_runner import make_commands, init_runner
+
+runner = init_runner(f'GPUCRL_CartPole_PLAN_HORIZON', num_threads=1)
+runner.num_workers = 1
+cmd_list = make_commands(
+    '../cart_pole/mbmppo.py',
+    base_args={'exploration': 'expected'},
+    fixed_hyper_args={},
+    common_hyper_args={
+        'plan-horizon': [0, 1, 4],
+        'seed': [1, 2, 3, 4]
+    },
+    algorithm_hyper_args={},
+)
+runner.run(cmd_list)
