7f267197bb2d835be87051801ecb303670e769fb
scuri <sebastian.curi@inf.ethz.ch>
2020-06-09 18:50:24 +0200

    Fix bug in scale method.

--- rllib/model/ensemble_model.py
+++ rllib/model/ensemble_model.py
@@ -53,7 +53,7 @@ class EnsembleModel(NNModel):
 
         # Compute epistemic uncertainty through moment matching.
         self.set_prediction_strategy('moment_matching')
-        _, scale = self.scale(state, action[..., :self.dim_action])
+        _, scale = self.forward(state, action[..., :self.dim_action])
 
         # Set state.
         self.set_prediction_strategy(prediction_strategy)
