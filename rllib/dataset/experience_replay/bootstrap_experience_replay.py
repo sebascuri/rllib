Fix gradient propagation in step function.
- Make bootstrap weights a torch tensor.
- Update pre-commit config.
- update setup.cfg

rllib/dataset/experience_replay/bootstrap_experience_replay.py
@@ -1,6 +1,5 @@
 """Implementation of an Experience Replay Buffer with a Bootstrap mask."""
 
-import numpy as np
 import torch
 from torch.distributions import Poisson
 
@@ -40,7 +39,7 @@ class BootstrapExperienceReplay(ExperienceReplay):
 
     def __init__(self, max_len, transformations=None, num_bootstraps=1, bootstrap=True):
         super().__init__(max_len, transformations)
-        self.weights = np.empty((self.max_len, num_bootstraps), dtype=np.int)
+        self.weights = torch.empty(self.max_len, num_bootstraps, dtype=torch.int)
         self.mask_distribution = Poisson(torch.ones(num_bootstraps))
         self.bootstrap = bootstrap
 

rllib/util/rollout.py
@@ -21,11 +21,14 @@ def step(environment, state, action, pi, render, goal=None):
     if not isinstance(action, torch.Tensor):
         action = torch.tensor(action, dtype=torch.get_default_dtype())
 
-    try:
-        entropy = pi.entropy().squeeze()
-    except NotImplementedError:
-        # Approximate it by MC sampling.
-        entropy = -pi.log_prob(action)
+    with torch.no_grad():
+        try:
+            entropy = pi.entropy().squeeze()
+        except NotImplementedError:
+            # Approximate it by MC sampling.
+            entropy = -pi.log_prob(action)
+
+        log_prob_action = pi.log_prob(action).squeeze()
 
     observation = RawObservation(
         state=state,
@@ -34,7 +37,7 @@ def step(environment, state, action, pi, render, goal=None):
         next_state=next_state,
         done=done,
         entropy=entropy,
-        log_prob_action=pi.log_prob(action).squeeze(),
+        log_prob_action=log_prob_action,
     ).to_torch()
     state = next_state
     if render: