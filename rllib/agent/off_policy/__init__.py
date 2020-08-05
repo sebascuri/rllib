38f7e2b61d99450e3dbd1a2bc61b33136dffdd3e
scuri <sebastian.curi@inf.ethz.ch>
2020-08-05 09:05:30 +0200

    Move Q-REPS to saddle_reps exp

--- rllib/agent/off_policy/__init__.py
+++ rllib/agent/off_policy/__init__.py
@@ -4,7 +4,6 @@ from .dpg_agent import DPGAgent
 from .dqn_learning_agent import DQNAgent
 from .mpo_agent import MPOAgent
 from .q_learning_agent import QLearningAgent
-from .q_reps_agent import QREPSAgent
 from .reps_agent import REPSAgent
 from .sac_agent import SACAgent
 from .soft_q_learning_agent import SoftQLearningAgent
