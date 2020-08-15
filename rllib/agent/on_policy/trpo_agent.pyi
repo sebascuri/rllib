a8aff986ce1e999e724fd5641291c57e9ac0a81e
scuri <sebastian.curi@inf.ethz.ch>
2020-08-15 18:04:46 +0200

    Add TRPO Agent stubs

---
+++ rllib/agent/on_policy/trpo_agent.pyi
@@ -0,0 +1,29 @@
+from typing import Any, Optional, Type, Union
+import torch.nn.modules.loss as loss
+
+from rllib.algorithms.trpo import TRPO
+from torch.nn.modules.loss import _Loss
+from torch.optim.optimizer import Optimizer
+from rllib.util.parameter_decay import ParameterDecay
+
+from rllib.policy import AbstractPolicy
+from rllib.value_function import AbstractValueFunction
+
+from .on_policy_agent import OnPolicyAgent
+
+class TRPOAgent(OnPolicyAgent):
+
+    algorithm: TRPO
+    def __init__(
+        self,
+        policy: AbstractPolicy,
+        value_function: AbstractValueFunction,
+        optimizer: Optimizer,
+        criterion: Type[_Loss],
+        regularization: bool = ...,
+        epsilon_mean: Union[float, ParameterDecay] = ...,
+        epsilon_var: Optional[Union[float, ParameterDecay]] = ...,
+        lambda_: float = ...,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None: ...
