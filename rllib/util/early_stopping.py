0c3ffafdb857ba8f9e340d7c3732ca6f3ba3c47f
scuri <sebastian.curi@inf.ethz.ch>
2020-09-04 09:25:55 +0200

    Add early stopping condition for non-decrease consequtive iterations

--- rllib/util/early_stopping.py
+++ rllib/util/early_stopping.py
@@ -5,6 +5,12 @@ from rllib.util.utilities import MovingAverage
 class EarlyStopping(object):
     """Early stopping algorithm.
 
+    Early stopping might raise the stop flag if:
+    a) The moving average of a quantity increases above a threshold. Or,
+    b) The moving average does not decrease for a set number of iterations.
+
+    The threshold can be set relative to a running minimum value, or a fixed quantity.
+
     Parameters
     ----------
     epsilon: float.
@@ -12,24 +18,30 @@ class EarlyStopping(object):
         If epsilon < 0 it will never raise the stop flag.
         If the algorithm did not start tracking, it will not raise the stop flag.
 
+    non_decrease_iter: int.
+        Number of consequtive non-decrease calls to `stop' that will raise the flag.
+
     relative: bool.
         Flag that indicates whether the stopping condition is relative or absolute.
         When relative, the stop flag is true when average > (1+epsilon) * minimum.
         When absolute, the stop flag is true when average > epsilon.
     """
 
-    def __init__(self, epsilon, relative=True):
+    def __init__(self, epsilon, non_decrease_iter=float("inf"), relative=True):
         self.epsilon = epsilon
         self.relative = relative
         self.moving_average = []
         self.min_value = []
+        self.non_decrease_iter = non_decrease_iter
+        self.count = 0
 
     @property
     def stop(self):
         """Return true if it is ready to early stop."""
         if self.min_value is None:
             return False
-        if self.epsilon < 0:
+        if self.epsilon < 0 and self.non_decrease_iter == float("inf"):
+            # No early stopping.
             return False
 
         for i, min_value in enumerate(self.min_value):
@@ -44,6 +56,12 @@ class EarlyStopping(object):
 
             if moving_average < min_value:
                 self.min_value[i] = moving_average
+                self.count = 0
+            else:
+                self.count += 1
+
+            if self.count >= self.non_decrease_iter:
+                return True
 
         return False
 
