d8606697e84ded87c957005205b2959939d9606a
scuri <sebastian.curi@inf.ethz.ch>
2020-09-06 15:52:54 +0200

    Average the MB target with the MF target using the sharpness

--- rllib/algorithms/mve.py
+++ rllib/algorithms/mve.py
@@ -2,6 +2,7 @@
 import torch
 
 from rllib.dataset.utilities import stack_list_of_tuples
+from rllib.util.training.utilities import sharpness
 from rllib.util.value_estimation import discount_cumsum, mc_return
 from rllib.value_function import NNEnsembleQFunction
 
@@ -68,6 +69,7 @@ def mve_expand(
 
         def get_value_target(self, observation):
             """Rollout model and call base algorithm with transitions."""
+            real_target_q = super().get_value_target(observation)
             if self.td_k:
                 if self.critic.discrete_state:
                     final_state = observation.next_state[..., -1]
@@ -83,7 +85,7 @@ def mve_expand(
                     (observation.reward, (final_value * (1 - done)).unsqueeze(-1)),
                     dim=-1,
                 )
-                target_q = discount_cumsum(
+                model_target_q = discount_cumsum(
                     rewards, self.gamma, self.reward_transformer
                 )[..., :-1]
 
@@ -91,14 +93,14 @@ def mve_expand(
                 with torch.no_grad():
                     state = observation.state[..., 0, :]
                     trajectory = self.simulate(state, self.policy)
-                    target_q = mc_return(
+                    model_target_q = mc_return(
                         trajectory,
                         gamma=self.gamma,
                         value_function=self.value_target,
                         reward_transformer=self.reward_transformer,
                     )
-                    target_q = (
-                        target_q.reshape(
+                    model_target_q = (
+                        model_target_q.reshape(
                             self.num_samples, *observation.state.shape[:-1], -1
                         )
                         .mean(0)
@@ -106,7 +108,11 @@ def mve_expand(
                     )
 
                     if isinstance(self.critic_target, NNEnsembleQFunction):
-                        target_q = torch.min(target_q, dim=-1)[0]
+                        model_target_q = torch.min(model_target_q, dim=-1)[0]
+
+            sharpness_ = sharpness(self.dynamical_model, observation)
+            alpha = 1.0 / (1.0 + self.num_steps * sharpness_)
+            target_q = alpha * model_target_q + (1 - alpha) * real_target_q
 
             return target_q
 
