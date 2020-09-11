37eeac99fe1b718241fbffda3b2c09e6e4d1c336
scuri <sebastian.curi@inf.ethz.ch>
2020-09-11 12:59:48 +0200

    Average arrays before logging

--- rllib/util/logger.py
+++ rllib/util/logger.py
@@ -83,6 +83,8 @@ class Logger(object):
             if isinstance(value, torch.Tensor):
                 value = value.detach().numpy()
             value = np.nan_to_num(value)
+            if isinstance(value, np.ndarray):
+                value = float(np.mean(value))
             if isinstance(value, np.float32):
                 value = float(value)
             if isinstance(value, np.int64):
