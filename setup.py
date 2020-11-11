a125b84ab1438d73f9809beff74a225432a35125
scuri <sebastian.curi@inf.ethz.ch>
2020-11-11 09:29:39 +0100

    Update setup.py with specific xml files

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     license="MIT",
     python_requires=">=3.7.0",
     packages=find_packages(exclude=["docs"]),
-    include_package_data=True,
+    package_data={"rllib": ["environment/mujoco/assets/*.xml"]},
     install_requires=[
         "numpy>=1.14,<2",
         "scipy>=1.3.0,<1.4.0",
