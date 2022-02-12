c2594dc5f4cb2f03afe59d1e815e43055a93a0e4
sukhijab <sukhijab@student.ethz.ch>
2022-02-12 22:11:08 +0100

    Adding new input transformations and layers argument for RNN models

--- rllib/util/input_transformations.py
+++ rllib/util/input_transformations.py
@@ -1,6 +1,8 @@
 """Input transformations for spot."""
 from abc import ABC, abstractmethod
 
+import torch
+
 
 class AbstractTransform(ABC):
     """Abstract Transformation definition."""
@@ -14,7 +16,7 @@ class AbstractTransform(ABC):
 
 
 class ComposeTransforms(AbstractTransform):
-    """Compose a list of transformations."""
+    """Compose transformations."""
 
     def __init__(self, transforms):
         super().__init__()
@@ -28,3 +30,39 @@ class ComposeTransforms(AbstractTransform):
         for transform in self.transforms:
             x = transform(x)
         return x
+
+
+class AngleToCosSin(AbstractTransform):
+    """Transform angles to CosSin."""
+
+    extra_dim = 1
+
+    @staticmethod
+    def __call__(x):
+        """Transform state before applying function approximation."""
+        angle, angular_velocity = torch.split(x, [1, x.shape[-1] - 1], dim=-1)
+        return torch.cat((torch.cos(angle), torch.sin(angle), angular_velocity), dim=-1)
+
+
+class RemoveAngle(AbstractTransform):
+    """Remove angle from state vector."""
+
+    extra_dim = -1
+
+    @staticmethod
+    def __call__(x):
+        """Transform state before applying function approximation."""
+        _, velocities = torch.split(x, [1, x.shape[-1] - 1], dim=-1)
+        return velocities
+
+
+class RemovePosition(AbstractTransform):
+    """Remove position from state vector."""
+
+    extra_dim = -2
+
+    @staticmethod
+    def __call__(x):
+        """Transform state before applying function approximation."""
+        _, other = torch.split(x, [2, x.shape[-1] - 2], -1)
+        return other
